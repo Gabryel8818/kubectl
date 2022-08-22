d4669fb3ef8df9ff84a5f6f97dc50837b1179b0c
gabryel8818 <gabriel_melo8818@hotmail.com>
2022-08-22 17:53:24 -0300

    feat: fix version bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 KUBECTL_VERSION=$2
 
 # Install kubectl 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl \
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl \
 && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 
